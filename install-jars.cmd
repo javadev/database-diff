4604815ba06d8bb238ad11e860231f9e1be9ed6a
Valentyn Kolesnikov <javadev75@gmail.com>
2020-07-29 16:38:26 +0000

    Added jar file to the sample ui application.

---
+++ install-jars.cmd
@@ -0,0 +1,1 @@
+mvn org.apache.maven.plugins:maven-install-plugin:2.5.2:install-file -Dfile=lib/oracle.sqldeveloper.jar -DgroupId=com.oracle -DartifactId=sqldeveloper -Dversion=1.0 -Dpackaging=jar -DgeneratePom=true
